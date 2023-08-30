f24714f15d3056a035b116b2c1c1c81abb13070d
namber0 <namnb2402@gmail.com>
2023-08-30 10:16:48 +0700

    updated documentation for perlin.py

--- algo-py/perlin.py
+++ algo-py/perlin.py
@@ -1,46 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# create a permutation table based on number of pixels seed is the initial value we want to start with
+# seed function is also used to get same set of numbers this helps to keep the perlin graph smooth
 def perlin(x, y, seed=0):
-    # create a permutation table based on number of pixels
-    # seed is the initial value we want to start with
-    # we also use seed function to get same set of numbers
-    # this helps to keep our perlin graph smooth
     np.random.seed(seed)
-    ptable = np.arange(256, dtype=int)
 
-    # shuffle our numbers in the table
+    ptable = np.arange(256, dtype=int)
     np.random.shuffle(ptable)
 
     # create a 2d array and then turn it one dimensional so that we can apply our dot product interpolations easily
     ptable = np.stack([ptable, ptable]).flatten()
-    
-    # grid coordinates
-    xi, yi = x.astype(int), y.astype(int)
-   
-    # distance vector coordinates
-    xg, yg = x - xi, y - yi
-    
-    # apply fade function to distance coordinates
+    xi, yi = x.astype(int), y.astype(int) # grid coordinates
+    xg, yg = x - xi, y - yi # distance vector coordinates
     xf, yf = fade(xg), fade(yg)
     
-    # the gradient vector coordinates in the top left, top right, bottom left bottom right
+    # the gradient vector coordinates
     n00 = gradient(ptable[ptable[xi] + yi], xg, yg)
     n01 = gradient(ptable[ptable[xi] + yi + 1], xg, yg - 1)
     n11 = gradient(ptable[ptable[xi + 1] + yi + 1], xg - 1, yg - 1)
     n10 = gradient(ptable[ptable[xi + 1] + yi], xg - 1, yg)
     
-    # apply linear interpolation i.e dot product to calculate average
+    # calculate average
     x1 = lerp(n00, n10, xf)
-    x2 = lerp(n01, n11, xf)  
+    x2 = lerp(n01, n11, xf)
+
     return lerp(x1, x2, yf)  
 
-# linear interpolation i.e dot product
 def lerp(a, b, x):
     return a + x * (b - a)
 
-# smoothing function,
-# the first derivative and second both are zero for this function
+# smoothing function
 def fade(f):
     return 6 * f**5 - 15 * f**4 + 10 * f**3
 
@@ -48,16 +38,13 @@ def fade(f):
 def gradient(c, x, y):
     vectors = np.array([[0, 1], [0, -1], [1, 0], [-1, 0]])
     gradient_co = vectors[c % 4]
+
     return gradient_co[:, :, 0] * x + gradient_co[:, :, 1] * y
 
-# create evenly spaced out numbers in a specified interval
-lin_array = np.linspace(1, 10, 500, endpoint=False)
 
-# create grid using linear 1d arrays
+lin_array = np.linspace(1, 10, 500, endpoint=False)
 x, y = np.meshgrid(lin_array, lin_array)  
 
-# generate graph
-plt.imshow(perlin(x, y, seed=2), origin = 'upper')
-
+plt.imshow(perlin(x, y, seed=1234), origin='lower')
 plt.show()
 
